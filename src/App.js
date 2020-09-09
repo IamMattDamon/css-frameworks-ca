c08fd915261a60f866bef088360f853c29308456
IamMattDamon <frochi@gmail.com>
2020-09-10 00:45:49 +0200

    trying to sort out sticky footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, NavLink, useRouteMatch, usePara
 import "./sass/style.scss";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
@@ -13,31 +14,33 @@ import Contact from "./components/contact/Contact";
 function App() {
   return (
     <Router>
-    <div>
-    
-    <div className="container">
-    <Navbar expand="lg" className="navbar navbar-expand-lg">
-        <Navbar.Brand href="/" className="heading">The YAY Company</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <NavLink exact to="/" className="nav-link px-5 px-lg-2">
+    <>
+    <div className="wrapper">
+    <Container>
+        <Navbar expand="lg" className="navbar navbar-expand-lg">
+            <Navbar.Brand href="/" className="heading">The YAY Company</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink exact to="/" className="nav-link px-5 px-lg-2">
                             Home
-                    </NavLink>
-                    <NavLink to="/news" className="nav-link">
+                        </NavLink>
+                        <NavLink to="/news" className="nav-link px-5 px-lg-2">
                             News
-                    </NavLink>
-                    <NavLink to="/contact" className="nav-link">
+                        </NavLink>
+                        <NavLink to="/contact" className="nav-link px-5 px-lg-2">
                             Contact
-                    </NavLink>
-                </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Go</Button>
-            </Form>
-        </Navbar.Collapse>
-    </Navbar>
-    </div>
+                        </NavLink>
+                    </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">
+                        Go
+                    </Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    </Container>
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
       <Switch>
@@ -53,21 +56,23 @@ function App() {
       </Switch>
 
       <footer className="footer">
-            <div className="container-lg d-flex justify-content-between align-items-center flex-column flex-md-row">
+            <Container className="container-lg d-flex justify-content-between align-items-center flex-column flex-md-row">  
                 <span className="py-4 py-md-2">
-                  <i className="fa fa-vimeo" aria-hidden="true"></i>
-                  <i className="fa fa-youtube-play" aria-hidden="true"></i>
+                  <i className="fa fa-vimeo mr-2" aria-hidden="true"></i>
+                  <i className="fa fa-youtube-play ml-2" aria-hidden="true"></i>
                 </span>
+                
                 <div className="d-flex d-block d-md-none flex-row w-100 justify-content-between">
                   <span>hello@yay.com</span>
                   <span className="ml-auto">Copyright 2020</span>
                 </div>
+                
                 <span className="d-none d-md-block">hello@yay.com</span>
                 <span className="d-none d-md-block">Copyright 2020</span>
-            </div>
+            </Container>
         </footer>
-
     </div>
+    </>
     </Router>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Container from "react-bootstrap/Container";
 
 
 function Home() {
     return (
     <div>
+      <main>
         <Carousel>
           <Carousel.Item>
             <img
@@ -28,6 +30,13 @@ function Home() {
             />
           </Carousel.Item>
         </Carousel>
+
+          <Container className="container-lg">
+              <h1 className="py-4">We do YAY things</h1>
+              <p className="pb-4">Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius eget massa.</p>
+          </Container>
+
+    </main>
     </div>
 );
 }
