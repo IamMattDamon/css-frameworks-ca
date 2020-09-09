185301a2392ad5e30e35ce5241d54dc258717754
IamMattDamon <frochi@gmail.com>
2020-09-09 14:33:41 +0200

    remove link index.html to style.css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-import Carousel from "react-bootstrap/Carousel";
+
 import Home from "./components/home/Home";
 import News from "./components/news/News";
 import Contact from "./components/contact/Contact";
@@ -15,52 +15,28 @@ function App() {
   return (
     <Router>
     <div>
-      <Navbar expand="lg">
+
+    <Navbar expand="lg">
         <Navbar.Brand href="/">The YAY Company</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-                <NavLink exact to="/" className="nav-link">
-                    Home
-                </NavLink>
-                <NavLink to="/news" className="nav-link">
-                    News
-                </NavLink>
-                <NavLink to="/contact" className="nav-link">
-                    Contact
-                </NavLink>
-            </Nav>
-        <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Go</Button>
-        </Form>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavLink exact to="/" className="nav-link">
+                            Home
+                    </NavLink>
+                    <NavLink to="/news" className="nav-link">
+                            News
+                    </NavLink>
+                    <NavLink to="/contact" className="nav-link">
+                            Contact
+                    </NavLink>
+                </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Go</Button>
+            </Form>
         </Navbar.Collapse>
-      </Navbar>
-
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="../images/carousel-1.jpg"
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="../images/carousel-2.jpg"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="../images/carousel-3.jpg"
-              alt="Third slide"
-            />
-          </Carousel.Item>
-        </Carousel>
-
+    </Navbar>
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
       <Switch>
@@ -74,6 +50,9 @@ function App() {
           <Contact />
         </Route>
       </Switch>
+
+          
+
     </div>
     </Router>
   );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
 
 function Home() {
-    return <div>Home</div>;
+    return (
+    <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../images/carousel-1.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../images/carousel-2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="../images/carousel-3.jpg"
+              alt="Third slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+    </div>
+);
 }
 
 export default Home;
