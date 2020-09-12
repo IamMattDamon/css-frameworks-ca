b8c14a79f8af7904063425802ecda98ff19911ff
IamMattDamon <frochi@gmail.com>
2020-09-12 04:38:21 +0200

    complete homepage

--- src/components/home/ContentTabs.js
+++ src/components/home/ContentTabs.js
@@ -10,33 +10,67 @@ function ContentTabs() {
         <>
         <Tabs defaultActiveKey="one" id="uncontrolled-tab-example">
             <Tab eventKey="one" title="First">
-            <Container className="mt-4">
-                <Row> 
-                <Col className="col-4">
-                <span className="tab-image">
-                    <img src="../images/old-television-1.jpg" className="tab-image" alt="old television"></img>
-                </span>  
-                </Col> 
-                <Col className="col-8">
-                <div>
-                    <span className="tab-paragraph">
-                        <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
-                    </span>
-                <div className="flex-lg-row">
-                    <span className="mr-4">SHARE</span>
-                    <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                    <span className="mr-4"><i className="fa fa-twitter"></i></span>
-                </div>
-                </div>
-                </Col>             
+            <Container className="mt-4 tabs-container">
+                <Row>
+                    <Col className="col-lg-3">
+                        <span className="tab-image">
+                            <img src="../images/old-television-1.jpg" className="tab-image" alt="old television"></img>
+                        </span>  
+                    </Col>
+                    <Col className="col-lg-8 col-md-6 ml-4">
+                        <span className="tab-paragraph">
+                            <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
+                        </span>
+                    <div className="flex-lg-row mt-5">
+                        <span className="mr-4">SHARE</span>
+                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
+                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                    </div>
+                </Col>
                 </Row>
             </Container>
             </Tab>
             <Tab eventKey="two" title="Second">
-                Tab 2
+            <Container className="mt-4 tabs-container">
+                <Row>
+                    <Col className="col-lg-3">
+                        <span className="tab-image">
+                            <img src="../images/old-television-2.jpg" className="tab-image" alt="old television"></img>
+                        </span>  
+                    </Col>
+                    <Col className="col-lg-8 col-md-6 ml-4">
+                        <span className="tab-paragraph">
+                            <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
+                        </span>
+                    <div className="flex-lg-row mt-5">
+                        <span className="mr-4">SHARE</span>
+                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
+                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                    </div>
+                </Col>
+                </Row>
+            </Container>
             </Tab>
             <Tab eventKey="three" title="Third">
-                Tab 3
+            <Container className="mt-4 tabs-container">
+                <Row>
+                    <Col className="col-lg-3">
+                        <span className="tab-image">
+                            <img src="../images/old-television-3.jpg" className="tab-image" alt="old television"></img>
+                        </span>  
+                    </Col>
+                    <Col className="col-lg-8 col-md-6 ml-4">
+                        <span className="tab-paragraph">
+                            <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
+                        </span>
+                    <div className="flex-lg-row mt-5">
+                        <span className="mr-4">SHARE</span>
+                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
+                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                    </div>
+                </Col>
+                </Row>
+            </Container>
             </Tab>
         </Tabs>
         </>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -38,7 +38,7 @@ function Home() {
             <p className="pb-4">Nam tincidunt, sapien nec congue porta, tellus risus ullamcorper mi, a rutrum justo eros pretium libero. Nullam vel enim id mauris eleifend finibus et ac orci. Aliquam metus massa, aliquam quis arcu sit amet, consectetur faucibus urna. Suspendisse massa diam, efficitur eu massa euismod, pretium lacinia magna. Donec mi orci, sollicitudin in luctus a, varius eget massa.</p>
         </Container>
 
-        <Container>
+        <Container className="mt-5">
             <ContentAccordion className="d-md-none" />
             <div className="d-none d-md-block">
                 <ContentTabs />
