c45ead06565c9f95986d4f1056f84a56e3c535e0
IamMattDamon <frochi@gmail.com>
2021-06-04 19:59:45 +0200

    responsiveness sorted for tabs section

--- src/components/home/ContentAccordion.js
+++ src/components/home/ContentAccordion.js
@@ -3,6 +3,9 @@ import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { FaFacebookF } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaShareAlt } from "react-icons/fa";
 
 
 function ContentAccordion() {
@@ -25,9 +28,9 @@ function ContentAccordion() {
                                 <img src="../images/old-television-1.jpg" className="accordion-image" alt="old television"></img>
                             </span>
                             <div className="flex-lg-row pt-4 pb-2 tab-icons">
-                                    <span className="mr-4">SHARE</span>
-                                    <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                                    <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                                <FaShareAlt className="tab-icons" />
+                                <FaFacebookF className="tab-icons ml-3"/>
+                                <AiOutlineTwitter className="tab-icons ml-3"/>
                             </div>
                         </Container>
                     </Card.Body>
@@ -41,7 +44,7 @@ function ContentAccordion() {
                     </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="1">
-                    <Card.Body className="home-accordion-background">
+                    <Card.Body className="home-accordion-background home-accordion-card">
                         <Container className="flex-md-column justify-content-center">
                             <span className="tab-paragraph">
                                 <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
@@ -50,9 +53,9 @@ function ContentAccordion() {
                                 <img src="../images/old-television-2.jpg" className="accordion-image" alt="old television"></img>
                             </span>
                             <div className="flex-lg-row pt-4 pb-2 tab-icons">
-                                    <span className="mr-4">SHARE</span>
-                                    <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                                    <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                                <FaShareAlt className="tab-icons" />
+                                <FaFacebookF className="tab-icons ml-3"/>
+                                <AiOutlineTwitter className="tab-icons ml-3"/>
                             </div>
                         </Container>
                     </Card.Body>
@@ -66,8 +69,8 @@ function ContentAccordion() {
                     </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="2">
-                    <Card.Body className="home-accordion-background">
-                        <Container className="flex-md-column justify-content-center">
+                    <Card.Body className="home-accordion-background home-accordion-card">
+                        <Container className="flex-md-column">
                             <span className="tab-paragraph">
                                 <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
                             </span>
@@ -75,9 +78,9 @@ function ContentAccordion() {
                                 <img src="../images/old-television-3.jpg" className="accordion-image" alt="old television"></img>
                             </span>
                             <div className="flex-lg-row pt-4 pb-2 tab-icons">
-                                    <span className="mr-4">SHARE</span>
-                                    <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                                    <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                                <FaShareAlt className="tab-icons" />
+                                <FaFacebookF className="tab-icons ml-3"/>
+                                <AiOutlineTwitter className="tab-icons ml-3"/>
                             </div>
                         </Container>
                     </Card.Body>
--- src/components/home/ContentTabs.js
+++ src/components/home/ContentTabs.js
@@ -4,6 +4,9 @@ import Tab from "react-bootstrap/Tab";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FaFacebookF } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaShareAlt } from "react-icons/fa";
 
 function ContentTabs() {
     return (
@@ -12,21 +15,21 @@ function ContentTabs() {
             <Tab eventKey="one" title="First">
             <Container className="mt-4 tabs-container">
                 <Row>
-                    <Col className="col-lg-3">
+                    <Col sm={5} lg={4}>
                         <span className="tab-image">
                             <img src="../images/old-television-1.jpg" className="tab-image" alt="old television"></img>
                         </span>  
                     </Col>
-                    <Col className="col-lg-8 col-md-6 ml-4">
+                    <Col sm={7} lg={8}>
                         <span className="tab-paragraph">
                             <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
                         </span>
-                    <div className="flex-lg-row mt-5">
-                        <span className="mr-4">SHARE</span>
-                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
-                    </div>
-                </Col>
+                        <div className="d-flex mt-4 row tabs-content-container">
+                            <FaShareAlt className="tab-icons" />
+                            <FaFacebookF className="tab-icons ml-3"/>
+                            <AiOutlineTwitter className="tab-icons ml-3"/>
+                        </div>
+                    </Col>
                 </Row>
             </Container>
             </Tab>
@@ -42,10 +45,10 @@ function ContentTabs() {
                         <span className="tab-paragraph">
                             <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
                         </span>
-                    <div className="flex-lg-row mt-5">
-                        <span className="mr-4">SHARE</span>
-                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                    <div className="flex-lg-row mt-5 row tabs-content-container">
+                            <FaShareAlt className="tab-icons" />
+                            <FaFacebookF className="tab-icons ml-3"/>
+                            <AiOutlineTwitter className="tab-icons ml-3"/>
                     </div>
                 </Col>
                 </Row>
@@ -63,10 +66,10 @@ function ContentTabs() {
                         <span className="tab-paragraph">
                             <p>Morbi eget efficitur turpis. Vivamus pellentesque tortor massa, venenatis pharetra leo laoreet a. Nullam non eleifend justo, a ullamcorper turpis. Cras vehicula pharetra lectus non maximus. Sed condimentum mattis rhoncus. </p>
                         </span>
-                    <div className="flex-lg-row mt-5">
-                        <span className="mr-4">SHARE</span>
-                        <span className="mr-4"><i className="fa fa-facebook-f"></i></span>
-                        <span className="mr-4"><i className="fa fa-twitter"></i></span>
+                    <div className="flex-lg-row mt-5 row tabs-content-container">
+                            <FaShareAlt className="tab-icons" />
+                            <FaFacebookF className="tab-icons ml-3"/>
+                            <AiOutlineTwitter className="tab-icons ml-3"/>
                     </div>
                 </Col>
                 </Row>
